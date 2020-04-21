b8a74cbd72c71ef3d66f6ea5d7e66d7f053c91b3
Tyler Beck <tbeck202@gmail.com>
2020-04-21 16:06:55 -0700

    Refactor app.js

--- checkers/app.js
+++ checkers/app.js
@@ -1,168 +1,105 @@
-//CREATE ROWS
-//CREATE SQUARES
-//ADD CLASS TO SQUARES
-const board = document.querySelector('.board');
-let squares = [];
-let rows = [];
-let redPieces = [];
-let blackPieces = [];
+const board = document.querySelector('.board'),
+	startGame = document.querySelector('#start');
+(rows = []), (squares = []), (pieces = []);
+
+//INITIATE TURN COUNTER AND TRACK TURNS
+let turnCounter;
+let turn;
 
 //CREATE ROWS
 for (let i = 0; i < 8; i++) {
 	const row = document.createElement('div');
-	for (let j = 0; j < 8; j++) {
-		const square = document.createElement('div');
-		square.classList.add('square');
-		row.append(square);
-		squares.push(square);
-	}
-	row.classList.add('row');
 	rows.push(row);
+	row.classList.add('row');
 	board.append(row);
 }
-
 //CREATE SQUARES
-squareCounter = 0;
-rows.forEach((row, rowIdx) => {
-	//SET COLOR OF SQUARES
-	if (rowIdx % 2 === 0) {
-		for (let i = 0; i < row.children.length; i++) {
-			if (i % 2 === 0) {
-				squares[squareCounter].classList.add('redSquare');
-			} else {
-				squares[squareCounter].classList.add('blackSquare');
-			}
-			squareCounter++;
-		}
-	} else {
-		for (let i = 0; i < row.children.length; i++) {
-			if (i % 2 !== 0) {
-				squares[squareCounter].classList.add('redSquare');
-			} else {
-				squares[squareCounter].classList.add('blackSquare');
-			}
-			squareCounter++;
-		}
-	}
-});
-
-//CREATE PIECES
 rows.forEach((row, idx) => {
-	if (idx < 2) {
-		const piece = document.createElement('div');
-		piece.classList.add('piece');
+	for (let i = 0; i < 8; i++) {
+		const square = document.createElement('div');
+		squares.push(square);
+		row.append(square);
+		square.classList.add('square');
+		formatSquares(square, idx, i);
 	}
 });
 
-squares.forEach((square, idx) => {
-	if (square.classList.contains('blackSquare') && idx < 16) {
-		const piece = document.createElement('div');
-		piece.classList.add('piece', 'redPiece');
-		square.append(piece);
-		redPieces.push(piece);
-	} else if (square.classList.contains('blackSquare') && idx > 47) {
-		const piece = document.createElement('div');
-		piece.classList.add('piece', 'blackPiece');
-		square.append(piece);
-		blackPieces.push(piece);
-	}
-});
-let allPieces = [ ...redPieces, ...blackPieces ];
-//GAME LOGIC
-
-let pieceSelected = false;
-//CURRENT POSITION OF SELECTED PIECE
-let selectedPiece;
-let selectedPieceIdx;
-//POSITION YOU'D LIKE TO MOVE TO
-let selectedSq;
-let selectedSqIdx;
-let selectedSqChildren;
-document.body.addEventListener('click', () => {
-	if (pieceSelected === true) {
-		board.addEventListener('click', (bEvt) => {
-			console.log('clicked');
-			let piece = bEvt;
-			// console.log(bEvt);
-			// BLACK PIECES MOVE LOGIC==========================
-			// let clickedRow = bEvt.target.parentElement;
-			// let validRow = selectedPiece.parentElement.parentElement.previousSibling;
-			// let moveSquareClasslist = bEvt.target.classList;
-			// if (pieceSelected && moveSquareClasslist.contains('blackSquare') && validRow === clickedRow) {
-			// 	bEvt.target.append(selectedPiece);
-			// 	console.log('valid row');
-			// }
-			//============================
-
-			blkMove(piece);
-		});
+//==FORMAT AND COLOR SQUARES
+function formatSquares(square, rowidx, squareidx) {
+	if (rowidx % 2 === 0) {
+		if (squareidx % 2 === 0) {
+			square.classList.add('redSquare');
+		} else {
+			square.classList.add('blackSquare');
+		}
 	} else {
-		console.log('Pick a piece');
+		if (squareidx % 2 === 0) {
+			square.classList.add('blackSquare');
+		} else {
+			square.classList.add('redSquare');
+		}
 	}
+}
+
+//CREATE PEICES
+for (let i = 0; i < 16; i++) {
+	const piece = document.createElement('div');
+	piece.classList.add('piece');
+	if (i < 8) {
+		piece.classList.add('redPiece');
+	} else {
+		piece.classList.add('blackPiece');
+	}
+	pieces.push(piece);
+}
+
+//==FORMAT AND COLOR PIECES
+startGame.addEventListener('click', () => {
+	let squareCount = 0;
+	squares.forEach((sq, idx) => {
+		if (sq.classList.contains('blackSquare') && idx < 16) {
+			sq.append(pieces[squareCount]);
+			squareCount++;
+		} else if (sq.classList.contains('blackSquare') && idx >= 48) {
+			sq.append(pieces[squareCount]);
+			squareCount++;
+		}
+	});
+	turnCounter = 2;
 });
 
-squares.forEach((sq, idx) => {
-	sq.addEventListener('click', (e) => {
-		selectedSq = sq;
-		selectedSqIdx = idx;
-		selectedSqChildren = e.target.children.length;
-		console.log(e);
-		// getRow(sq);
-		let eClassList = e.target.classList;
-		// console.log(idx);
-		//SELECT PIECE LOGIC=========================
-		if (eClassList.value.includes('piece') && !eClassList.value.includes('selected') && selectedSqChildren === 0) {
-			allPieces.forEach((pce) => {
-				pce.classList.remove('selected');
-			});
-			// console.log('Piece selected', e);
-			eClassList.toggle('selected');
-			pieceSelected = true;
-			selectedPiece = e.target;
-			selectedPieceIdx = idx;
-			// console.log(selectedPiece.parentElement);
-			//DE-SELECT PIECE LOGIC=====================================
-		} else if (eClassList.value.includes('selected')) {
-			eClassList.remove('selected');
-			pieceSelected = false;
-			selectedPieceIdx = undefined;
-		} //else if (pieceSelected && eClassList.value.includes('blackSquare')) {
-		// 	e.target.append(selectedPiece);
-		// }
-		// console.log('Piece selected', pieceSelected);
+//	SELECT PIECE TO MOVE
+pieces.forEach((p, idx) => {
+	p.addEventListener('click', (e) => {
+		if (turnCounter % 2 === 0) {
+			turn = 'black';
+		} else if (turnCounter % 2 !== 0) {
+			turn = 'red';
+		}
+		let piece = e.target;
+		pieces.forEach((pp, idx) => {
+			if (piece.classList.contains(`${turn}Piece`)) {
+				pp.classList.remove('selected');
+				piece.classList.add('selected');
+			}
+		});
 	});
 });
 
-const blkMove = (piece) => {
-	let clickedRow = piece.target.parentElement;
-	let validRow = selectedPiece.parentElement.parentElement.previousSibling;
-	let moveSquareClasslist = piece.target.classList;
-	if (
-		pieceSelected &&
-		moveSquareClasslist.contains('blackSquare') &&
-		validRow === clickedRow &&
-		validSq() &&
-		selectedSqChildren === 0
-	) {
-		piece.target.append(selectedPiece);
-		selectedPiece.classList.remove('selected');
-		selectedPiece = undefined;
-		selectedPieceIdx = undefined;
-		selectedSq = undefined;
-		selectedSqIdx = undefined;
-		pieceSelected = false;
-		console.log('valid row');
-	}
-};
+// SELECT SQUARE TO MOVE TO
+board.addEventListener('click', (e) => {
+	// console.log(e);
+	let desiredMove = e.target;
+	pieces.forEach((p, idx) => {
+		if (p.classList.contains('selected') && !desiredMove.classList.contains('piece')) {
+			desiredMove.append(p);
+			p.classList.remove('selected');
+		}
+	});
+});
 
-const validSq = () => {
-	if (selectedPieceIdx - selectedSqIdx === 9 || selectedPieceIdx - selectedSqIdx === 7) {
-		return true;
-	} else {
-		return false;
-	}
-};
+//SET PLAYER TURN
 
-// const getRow = (sq) => {
-// 	rows.forEach((row) => {});
-// };
+//CREATE MOVE LOGIC
+//CREATE PIECE CAPTURE LOGIC
+//CREATE WIN LOGIC
---
+++ checkers/original-app.js
@@ -0,0 +1,158 @@
+//CREATE ROWS
+//CREATE SQUARES
+//ADD CLASS TO SQUARES
+const board = document.querySelector('.board');
+let squares = [];
+let rows = [];
+let redPieces = [];
+let blackPieces = [];
+
+//CREATE ROWS
+for (let i = 0; i < 8; i++) {
+	const row = document.createElement('div');
+	for (let j = 0; j < 8; j++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+		row.append(square);
+		squares.push(square);
+	}
+	row.classList.add('row');
+	rows.push(row);
+	board.append(row);
+}
+
+//CREATE SQUARES
+squareCounter = 0;
+rows.forEach((row, rowIdx) => {
+	//SET COLOR OF SQUARES
+	if (rowIdx % 2 === 0) {
+		for (let i = 0; i < row.children.length; i++) {
+			if (i % 2 === 0) {
+				squares[squareCounter].classList.add('redSquare');
+			} else {
+				squares[squareCounter].classList.add('blackSquare');
+			}
+			squareCounter++;
+		}
+	} else {
+		for (let i = 0; i < row.children.length; i++) {
+			if (i % 2 !== 0) {
+				squares[squareCounter].classList.add('redSquare');
+			} else {
+				squares[squareCounter].classList.add('blackSquare');
+			}
+			squareCounter++;
+		}
+	}
+});
+
+//CREATE PIECES
+rows.forEach((row, idx) => {
+	if (idx < 2) {
+		const piece = document.createElement('div');
+		piece.classList.add('piece');
+	}
+});
+
+squares.forEach((square, idx) => {
+	if (square.classList.contains('blackSquare') && idx < 16) {
+		const piece = document.createElement('div');
+		piece.classList.add('piece', 'redPiece');
+		square.append(piece);
+		redPieces.push(piece);
+	} else if (square.classList.contains('blackSquare') && idx > 47) {
+		const piece = document.createElement('div');
+		piece.classList.add('piece', 'blackPiece');
+		square.append(piece);
+		blackPieces.push(piece);
+	}
+});
+let allPieces = [ ...redPieces, ...blackPieces ];
+//GAME LOGIC
+
+let pieceSelected = false;
+//CURRENT POSITION OF SELECTED PIECE
+let selectedPiece;
+let selectedPieceIdx;
+//POSITION YOU'D LIKE TO MOVE TO
+let selectedSq;
+let selectedSqIdx;
+let selectedSqChildren;
+document.body.addEventListener('click', () => {
+	if (pieceSelected === true) {
+		board.addEventListener('click', (bEvt) => {
+			console.log('clicked');
+			let piece = bEvt;
+
+			blkMove(piece);
+		});
+	} else {
+		console.log('Pick a piece');
+	}
+});
+
+squares.forEach((sq, idx) => {
+	sq.addEventListener('click', (e) => {
+		selectedSq = sq;
+		selectedSqIdx = idx;
+		selectedSqChildren = e.target.children.length;
+		console.log(e);
+		// getRow(sq);
+		let eClassList = e.target.classList;
+		// console.log(idx);
+		//SELECT PIECE LOGIC=========================
+		if (eClassList.value.includes('piece') && !eClassList.value.includes('selected') && selectedSqChildren === 0) {
+			allPieces.forEach((pce) => {
+				pce.classList.remove('selected');
+			});
+			// console.log('Piece selected', e);
+			eClassList.toggle('selected');
+			pieceSelected = true;
+			selectedPiece = e.target;
+			selectedPieceIdx = idx;
+			// console.log(selectedPiece.parentElement);
+			//DE-SELECT PIECE LOGIC=====================================
+		} else if (eClassList.value.includes('selected')) {
+			eClassList.remove('selected');
+			pieceSelected = false;
+			selectedPieceIdx = undefined;
+		} //else if (pieceSelected && eClassList.value.includes('blackSquare')) {
+		// 	e.target.append(selectedPiece);
+		// }
+		// console.log('Piece selected', pieceSelected);
+	});
+});
+
+const blkMove = (piece) => {
+	let clickedRow = piece.target.parentElement;
+	let validRow = selectedPiece.parentElement.parentElement.previousSibling;
+	let moveSquareClasslist = piece.target.classList;
+	if (
+		pieceSelected &&
+		moveSquareClasslist.contains('blackSquare') &&
+		validRow === clickedRow &&
+		validSq() &&
+		selectedSqChildren === 0
+	) {
+		piece.target.append(selectedPiece);
+		selectedPiece.classList.remove('selected');
+		selectedPiece = undefined;
+		selectedPieceIdx = undefined;
+		selectedSq = undefined;
+		selectedSqIdx = undefined;
+		pieceSelected = false;
+		console.log('valid row');
+	}
+};
+
+const validSq = () => {
+	if (selectedPieceIdx - selectedSqIdx === 9 || selectedPieceIdx - selectedSqIdx === 7) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+// const getRow = (sq) => {
+// 	rows.forEach((row) => {});
+// };
